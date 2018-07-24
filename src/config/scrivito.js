6853645c62cc751dcb46c3d9e5fb30f5f73d88e4
Agsiegert <agsiegert@hotmail.com>
2018-07-24 12:59:24 +0200

    unique routing

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -2,5 +2,5 @@ import * as Scrivito from 'scrivito';
 
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
-  homepage: () => Scrivito.Obj.getByPath('/product'),
+  homepage: () => Scrivito.Obj.getByPath(location.origin.match(/shi.netlify.com/) ? '/product' : '/'),
 });
