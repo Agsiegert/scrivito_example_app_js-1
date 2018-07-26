2169477a8cfa37fd4940ac20774936fe4d44a374
Agsiegert <agsiegert@hotmail.com>
2018-07-26 13:43:37 +0200

    switch3

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,22 +1,22 @@
 import * as Scrivito from 'scrivito';
-
-var origin = location.origin;
-switch (origin) {
-  case 'https://shi.netlify.com' :
-    'my-product';
-    break;
-  case 'https://www.safehomeinspection.com' :
-    'about';
-    break;
-  case 'https://www.homeinspectionlink.com' :
-    'about';
-    break;
-  default:
-    'home';
-}
 // location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'
 
-Scrivito.configure({
+Scrivito.configure(
+	var origin = location.origin;
+	switch (origin) {
+	  case 'https://shi.netlify.com' :
+	    'my-product';
+	    break;
+	  case 'https://www.safehomeinspection.com' :
+	    'about';
+	    break;
+	  case 'https://www.homeinspectionlink.com' :
+	    'about';
+	    break;
+	  default:
+	    'home';
+	}
+{
   tenant: process.env.SCRIVITO_TENANT,
   homepage: () => Scrivito.Obj.getByPermalink(origin),
 });
