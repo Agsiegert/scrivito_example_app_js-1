a7ca57794f402bb6c9ac5e41222cdb083a5a3ef7
Agsiegert <agsiegert@hotmail.com>
2018-07-26 13:22:45 +0200

    no path and permalink test

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -14,5 +14,5 @@ import * as Scrivito from 'scrivito';
 
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
-  homepage: () => Scrivito.Obj.getByPath(location.origin.match(/shi.netlify.com/) ? '/product' : '/about'),
+  homepage: () => Scrivito.Obj.getByPermalink(location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'),
 });
