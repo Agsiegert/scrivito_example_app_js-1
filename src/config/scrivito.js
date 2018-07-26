0e5879b467e2417e68d071c4fd9695b1f027065a
Agsiegert <agsiegert@hotmail.com>
2018-07-26 13:36:19 +0200

    switch2

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,6 +1,24 @@
 import * as Scrivito from "scrivito";
 
+<<<<<<< HEAD
 const config = { tenant: process.env.SCRIVITO_TENANT };
+=======
+var origin = location.origin;
+switch (origin) {
+  case 'https://shi.netlify.com' :
+    'my-product';
+    break;
+  case 'https://www.safehomeinspection.com' :
+    'about';
+    break;
+  case 'https://www.homeinspectionlink.com' :
+    'about';
+    break;
+  default:
+    'home';
+}
+// location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'
+>>>>>>> switch2
 
 <<<<<<< HEAD
 if (process.env.SCRIVITO_ORIGIN) {
@@ -11,6 +29,6 @@ Scrivito.configure(config);
 =======
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
-  homepage: () => Scrivito.Obj.getByPermalink(location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'),
+  homepage: () => Scrivito.Obj.getByPermalink(origin),
 });
 >>>>>>> no path and permalink test
