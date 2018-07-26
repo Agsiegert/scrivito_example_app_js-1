7aead4df0ca5e9a1a4f672182509829a5b706f66
Agsiegert <agsiegert@hotmail.com>
2018-07-26 13:22:45 +0200

    no path and permalink test

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -2,8 +2,15 @@ import * as Scrivito from "scrivito";
 
 const config = { tenant: process.env.SCRIVITO_TENANT };
 
+<<<<<<< HEAD
 if (process.env.SCRIVITO_ORIGIN) {
   config.origin = process.env.SCRIVITO_ORIGIN;
 }
 
 Scrivito.configure(config);
+=======
+Scrivito.configure({
+  tenant: process.env.SCRIVITO_TENANT,
+  homepage: () => Scrivito.Obj.getByPermalink(location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'),
+});
+>>>>>>> no path and permalink test
