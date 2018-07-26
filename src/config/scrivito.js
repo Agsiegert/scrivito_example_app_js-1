a64ca1e86775a592a258295003075decdf6dec64
Agsiegert <agsiegert@hotmail.com>
2018-07-26 13:36:19 +0200

    switch2

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,18 +1,22 @@
 import * as Scrivito from 'scrivito';
 
-// var origin = location.origin;
-// switch (origin) {
-//   case 'shi.netlify.com' :
-//     '/product';
-//     break;
-//   case 'www.safehomeinspection.com' :
-//     '/';
-//     break;
-//   default:
-//     '';
-// }
+var origin = location.origin;
+switch (origin) {
+  case 'https://shi.netlify.com' :
+    'my-product';
+    break;
+  case 'https://www.safehomeinspection.com' :
+    'about';
+    break;
+  case 'https://www.homeinspectionlink.com' :
+    'about';
+    break;
+  default:
+    'home';
+}
+// location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'
 
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
-  homepage: () => Scrivito.Obj.getByPermalink(location.origin.match(/shi.netlify.com/) ? 'my-product' : 'no-path'),
+  homepage: () => Scrivito.Obj.getByPermalink(origin),
 });
