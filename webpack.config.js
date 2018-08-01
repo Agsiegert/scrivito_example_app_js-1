fc6e540afd8fc9f9bf971304a4f267bd35668828
Antony Siegert <agsiegert@hotmail.com>
2018-08-01 12:26:02 +0200

    Updating config to prevent error from updating uglifyjs-webpack-plugin from 1.2.5
    changes to uglifyjs-webpack-plugin 1.2.6 creates an error when loading the example app in production mode.  This change solves the issue.  uglifyjs-webpack-plugin should be monitored and this change reverted when updated.

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,9 @@ module.exports = (env = {}) => {
         uglifyOptions: {
           ie8: false,
           ecma: 5,
+          compress: {
+            inline: true, // prevents error when upgrading from uglifyjs-webpack-plugin 1.2.5
+          },
         },
       }),
       new ZipPlugin({
