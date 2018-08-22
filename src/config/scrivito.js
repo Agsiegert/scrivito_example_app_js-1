71fc1716b95b23b5211542084d6ec7d0e61dc7f9
Antony Siegert <antony.siegert@infopark.de>
2018-08-22 18:00:55 +0200

    revert domain switch test

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,22 +1,5 @@
 import * as Scrivito from "scrivito";
 
-function rootObj() {
-  var rootPath = '/';
-  switch (location.hostname) {
-    case 'shi.netlify.com' :
-      rootPath = '/product';
-      break;
-    case 'www.safehomeinspection.com' :
-      rootPath = '/about';
-      break;
-    case 'homeinspectionlink.com' :
-      rootPath = '/';
-      break;
-  }
-  return Scrivito.Obj.getByPath(rootPath);
-}
-
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
-  homepage: () => rootObj(),
 });
