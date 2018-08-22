5113aa0c7aeafc08c8c015ed96d08b04b68a20f5
Antony Siegert <antony.siegert@infopark.de>
2018-08-22 17:39:30 +0200

    test andreas multisite switch

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,5 +1,22 @@
 import * as Scrivito from "scrivito";
 
+function rootObj {
+  var rootPath = '/';
+  switch (location.hostname) {
+    case 'shi.netlify.com' :
+      rootPath = '/my-product';
+      break;
+    case 'www.safehomeinspection.com' :
+      rootPath = '/no-path';
+      break;
+    case 'homeinspectionlink.com' :
+      rootPath = '/';
+      break;
+  }
+  return Scrivito.Obj.getByPath(rootPath);
+}
+
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
+  homepage: () => rootObj(),
 });
