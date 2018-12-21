2b97a79d851e3c99e796376e7cc162060dca5e61
Agsiegert <agsiegert@hotmail.com>
2018-12-22 00:06:33 +0100

    Try head script optimization

--- src/Widgets/ImageWidget/ImageWidgetComponent.js
+++ src/Widgets/ImageWidget/ImageWidgetComponent.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
 import AnimateOnReveal from "../../Components/AnimateOnReveal";
-import 'lazysizes';
-import 'lazysizes/plugins/attrchange/ls.attrchange';
 
 Scrivito.provideComponent("ImageWidget", ({ widget }) => {
   let image = (
