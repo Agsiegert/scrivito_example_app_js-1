b50c95e94cfb0ada250755c028bcc3d196083b4a
Agsiegert <agsiegert@hotmail.com>
2018-12-27 10:04:21 +0100

    Defer visibility of section widgets

--- src/Widgets/SectionWidget/SectionWidgetComponent.js
+++ src/Widgets/SectionWidget/SectionWidgetComponent.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
+import TrackVisibility from "react-on-screen";
+import { textExtractFromWidgetlist } from "../../utils/textExtract";
 
 Scrivito.provideComponent("SectionWidget", ({ widget }) => {
   const sectionClassNames = [];
+
   const sectionStyle = {};
 
   let backgroundColor = widget.get("backgroundColor") || "white";
@@ -24,19 +27,23 @@ Scrivito.provideComponent("SectionWidget", ({ widget }) => {
     sectionClassNames.push("no-padding");
   }
 
-  let contentClassName = "container";
+  let contentClassNames = ["container"];
+
   if (widget.get("useFullWidth") === "yes") {
-    contentClassName = "container-fluid gutter0";
+    contentClassNames = ["container-fluid gutter0"];
   }
 
   if (widget.get("useFullHeight") === "yes") {
     sectionClassNames.push("full-height");
   }
 
-  return (
+  const sectionClassName = sectionClassNames.join(" ");
+  const contentClassName = contentClassNames.join(" ");
+
+  const section = (
     <Scrivito.BackgroundImageTag
       tag="section"
-      className={sectionClassNames.join(" ")}
+      className={sectionClassName}
       style={sectionStyle}
     >
       <Scrivito.ContentTag
@@ -46,4 +53,24 @@ Scrivito.provideComponent("SectionWidget", ({ widget }) => {
       />
     </Scrivito.BackgroundImageTag>
   );
+
+  if (widget.get("deferVisibility") === "no") {
+    return section;
+  }
+
+  return (
+    <DeferredVisibility visible={section}>
+      <div className={sectionClassName}>
+        <p className={contentClassName}>
+          {textExtractFromWidgetlist(widget.get("content"))}
+        </p>
+      </div>
+    </DeferredVisibility>
+  );
 });
+
+const DeferredVisibility = ({ children, visible }) => (
+  <TrackVisibility once partialVisibility offset={1200}>
+    {({ isVisible }) => (isVisible ? visible : children)}
+  </TrackVisibility>
+);
