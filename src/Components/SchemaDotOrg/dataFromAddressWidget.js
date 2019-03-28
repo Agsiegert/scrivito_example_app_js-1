1342fb79f3e11d5a30c804dc1b96d7b6823437cd
Antony Siegert <antony.siegert@infopark.de>
2019-03-28 16:20:47 +0100

    test place

--- src/Components/SchemaDotOrg/dataFromAddressWidget.js
+++ src/Components/SchemaDotOrg/dataFromAddressWidget.js
@@ -2,21 +2,27 @@ function dataFromAddressWidget(addressWidget) {
   return {
     "@type": "Place",
     name: addressWidget.get("locationName"),
-    address: addressFromAddressWidget(addressWidget),
+    address: { "@type": "PostalAddress",
+      streetAddress: addressWidget.get("locationStreetAddress"),
+      addressLocality: addressWidget.get("locationLocality"),
+      addressRegion: addressWidget.get("locationRegion"),
+      postalCode: addressWidget.get("locationPostalCode"),
+      addressCountry: addressWidget.get("locationCountry"),
+    },
     telephone: addressWidget.get("phone"),
     faxNumber: addressWidget.get("fax"),
   };
 }
 
-function addressFromAddressWidget(addressWidget) {
-  return {
-    "@type": "PostalAddress",
-    streetAddress: addressWidget.get("locationStreetAddress"),
-    addressLocality: addressWidget.get("locationLocality"),
-    addressRegion: addressWidget.get("locationRegion"),
-    postalCode: addressWidget.get("locationPostalCode"),
-    addressCountry: addressWidget.get("locationCountry"),
-  };
-}
+// function addressFromAddressWidget(addressWidget) {
+//   return {
+//     "@type": "PostalAddress",
+//     streetAddress: addressWidget.get("locationStreetAddress"),
+//     addressLocality: addressWidget.get("locationLocality"),
+//     addressRegion: addressWidget.get("locationRegion"),
+//     postalCode: addressWidget.get("locationPostalCode"),
+//     addressCountry: addressWidget.get("locationCountry"),
+//   };
+// }
 
 export default dataFromAddressWidget;
