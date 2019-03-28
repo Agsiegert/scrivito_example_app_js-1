723b9071908baa0d0b93d16e4ff5074c91461a56
Antony Siegert <antony.siegert@infopark.de>
2019-03-28 14:36:56 +0100

    add quotes

--- src/Components/SchemaDotOrg/dataFromJob.js
+++ src/Components/SchemaDotOrg/dataFromJob.js
@@ -5,36 +5,36 @@ function dataFromJob(job) {
   return {
     "@context": "http://schema.org",
     "@type": "JobPosting",
-    title: job.get("title"),
-    datePosted: formatDate(job.get("datePosted"), "yyyy-mm-dd"),
-    validThrough: formatDate(job.get("validThrough"), "yyyy-mm-dd"),
-    description: textExtractFromWidgetlist(job.get("body")),
-    employmentType: job.get("employmentType"),
-    hiringOrganization: {
+    "title": job.get("title"),
+    "datePosted": formatDate(job.get("datePosted"), "yyyy-mm-dd"),
+    "validThrough": formatDate(job.get("validThrough"), "yyyy-mm-dd"),
+    "description": textExtractFromWidgetlist(job.get("body")),
+    "employmentType": job.get("employmentType"),
+    "hiringOrganization": {
       "@type": "Organization",
-      name: job.get("hiringOrganizationName"),
-      sameAs: job.get("hiringOrganizationWebsite"),
+      "name": job.get("hiringOrganizationName"),
+      "sameAs": job.get("hiringOrganizationWebsite"),
     },
-    jobLocation: locationFromJob(job),
+    "jobLocation": locationFromJob(job),
   };
 }
 
 function locationFromJob(job) {
   return {
     "@type": "Place",
-    name: job.get("locationName"),
-    address: addressFromJob(job),
+    "name": job.get("locationName"),
+    "address": addressFromJob(job),
   };
 }
 
 function addressFromJob(job) {
   return {
     "@type": "PostalAddress",
-    streetAddress: job.get("locationStreetAddress"),
-    addressLocality: job.get("locationLocality"),
-    addressRegion: job.get("locationRegion"),
-    postalCode: job.get("locationPostalCode"),
-    addressCountry: job.get("locationCountry"),
+    "streetAddress": job.get("locationStreetAddress"),
+    "addressLocality": job.get("locationLocality"),
+    "addressRegion": job.get("locationRegion"),
+    "postalCode": job.get("locationPostalCode"),
+    "addressCountry": job.get("locationCountry"),
   };
 }
 
