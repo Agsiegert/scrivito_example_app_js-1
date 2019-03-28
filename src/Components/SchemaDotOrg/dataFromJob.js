8ddbcf1ce259343b2070b0b071848d54a7489537
Antony Siegert <antony.siegert@infopark.de>
2019-03-28 17:11:08 +0100

    string literal

--- src/Components/SchemaDotOrg/dataFromJob.js
+++ src/Components/SchemaDotOrg/dataFromJob.js
@@ -2,40 +2,40 @@ import formatDate from "../../utils/formatDate";
 import { textExtractFromWidgetlist } from "../../utils/textExtract";
 
 function dataFromJob(job) {
-  return {
+  return `{
     "@context": "http://schema.org",
     "@type": "JobPosting",
-    "title": job.get("title"),
-    "datePosted": formatDate(job.get("datePosted"), "yyyy-mm-dd"),
-    "validThrough": formatDate(job.get("validThrough"), "yyyy-mm-dd"),
-    "description": textExtractFromWidgetlist(job.get("body")),
-    "employmentType": job.get("employmentType"),
+    "title": ${job.get("title")},
+    "datePosted": ${formatDate(job.get("datePosted"), "yyyy-mm-dd")},
+    "validThrough": ${formatDate(job.get("validThrough"), "yyyy-mm-dd")},
+    "description": ${textExtractFromWidgetlist(job.get("body"))},
+    "employmentType": ${job.get("employmentType")},
     "hiringOrganization": {
       "@type": "Organization",
-      "name": job.get("hiringOrganizationName"),
-      "sameAs": job.get("hiringOrganizationWebsite")
+      "name": ${job.get("hiringOrganizationName")},
+      "sameAs": ${job.get("hiringOrganizationWebsite")}
     },
-    "jobLocation": locationFromJob(job)
-  };
+    "jobLocation": ${locationFromJob(job)}
+  }`;
 }
 
 function locationFromJob(job) {
-  return {
+  return `{
     "@type": "Place",
-    "name": job.get("locationName"),
-    "address": addressFromJob(job)
-  };
+    "name": ${job.get("locationName")},
+    "address": ${addressFromJob(job)}
+  }`;
 }
 
 function addressFromJob(job) {
-  return {
+  return `{
     "@type": "PostalAddress",
-    "streetAddress": job.get("locationStreetAddress"),
-    "addressLocality": job.get("locationLocality"),
-    "addressRegion": job.get("locationRegion"),
-    "postalCode": job.get("locationPostalCode"),
-    "addressCountry": job.get("locationCountry")
-  };
+    "streetAddress": ${job.get("locationStreetAddress")},
+    "addressLocality": ${job.get("locationLocality")},
+    "addressRegion": ${job.get("locationRegion")},
+    "postalCode": ${job.get("locationPostalCode")},
+    "addressCountry": ${job.get("locationCountry")}
+  }`;
 }
 
 export default dataFromJob;
