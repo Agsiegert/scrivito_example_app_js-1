cd1d81bb83434615704d473d398483dcc4fcc950
Antony Siegert <antony.siegert@infopark.de>
2019-03-28 18:06:10 +0100

    test

--- src/Components/SchemaDotOrg.js
+++ src/Components/SchemaDotOrg.js
@@ -10,9 +10,9 @@ import dataFromBlogPost from "./SchemaDotOrg/dataFromBlogPost";
 import dataFromAddressWidget from "./SchemaDotOrg/dataFromAddressWidget";
 
 const SchemaDotOrg = Scrivito.connect(({ content }) => {
-  const data = pruneEmptyValues(JSON.stringify(dataFromItem(content)));
+  const data = pruneEmptyValues(dataFromItem(content));
 
-  return <script type="application/ld+json">{data}</script>;
+  return <script type="application/ld+json">{JSON.parse(data)}</script>;
 });
 
 function dataFromItem(item) {
