a4de345a52f091a874d62357b45a88df0b60405e
Antony Siegert <antony.siegert@infopark.de>
2019-03-29 14:03:18 +0100

    add replace

--- src/Components/SchemaDotOrg.js
+++ src/Components/SchemaDotOrg.js
@@ -12,7 +12,7 @@ import dataFromAddressWidget from "./SchemaDotOrg/dataFromAddressWidget";
 const SchemaDotOrg = Scrivito.connect(({ content }) => {
   const data = pruneEmptyValues(dataFromItem(content));
 
-  return <script type="application/ld+json">$!{JSON.stringify(data)}</script>;
+  return <script type="application/ld+json">{JSON.stringify(data).replace(/<\//g, '<\\u002F')}</script>;
 });
 
 function dataFromItem(item) {
