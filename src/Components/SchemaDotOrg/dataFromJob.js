f6e53693ab599ee49633b46a1a251b03c19c74a8
Antony Siegert <antony.siegert@infopark.de>
2019-04-01 11:16:06 +0200

    remove tick

--- src/Components/SchemaDotOrg/dataFromJob.js
+++ src/Components/SchemaDotOrg/dataFromJob.js
@@ -2,7 +2,7 @@ import formatDate from "../../utils/formatDate";
 import { textExtractFromWidgetlist } from "../../utils/textExtract";
 
 function dataFromJob(job) {
-  return (`{
+  return `{
     \"@context\": \"http://schema.org\",
     \"@type\": \"JobPosting\",
     \"title\": ${job.get("title")},
@@ -16,26 +16,26 @@ function dataFromJob(job) {
       \"sameAs\": ${job.get("hiringOrganizationWebsite")}
     },
     \"jobLocation\": ${locationFromJob(job)}
-  }`);
+  }`;
 }
 
 function locationFromJob(job) {
-  return (`{
+  return `{
     \"@type\": \"Place\",
     \"name\": ${job.get("locationName")},
     \"address\": ${addressFromJob(job)}
-  }`);
+  }`;
 }
 
 function addressFromJob(job) {
-  return (`{
+  return `{
     \"@type\": \"PostalAddress\",
     \"streetAddress\": ${job.get("locationStreetAddress")},
     \"addressLocality\": ${job.get("locationLocality")},
     \"addressRegion\": ${job.get("locationRegion")},
     \"postalCode\": ${job.get("locationPostalCode")},
     \"addressCountry\": ${job.get("locationCountry")}
-  }`);
+  }`;
 }
 
 export default dataFromJob;
