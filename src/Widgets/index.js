c33e08025440ce35c1d2791fa443dcf3409f7511
Antony Siegert <antony.siegert@infopark.de>
2019-05-02 11:18:23 +0200

    update widgets

--- src/Widgets/index.js
+++ src/Widgets/index.js
@@ -1,5 +1,5 @@
-import "scrivito-collapsible-card";
-import "scrivito-collapsible-card/index.css";
+// import "scrivito-collapsible-card";
+// import "scrivito-collapsible-card/index.css";
 import "scrivito-areachart";
 import "scrivito-adv-button";
 import "scrivito-adv-button/index.css";
@@ -10,13 +10,12 @@ import "scrivito-highlight-code/index.css";
 import "scrivito-barchart";
 import "scrivito-accessible-accordion";
 import "scrivito-reference-teaser";
-import "scrivito-eyecatcher";
-import "scrivito-eyecatcher/index.css";
 import "scrivito-countdown";
 import "scrivito-time-ago";
 import "scrivito-follow-on-twitter";
 import "scrivito-twitter-timeline";
 import "scrivito-progressbar";
+import "scrivito-progressbar/index.css";
 import "scrivito-linechart";
 import "scrivito-piechart";
 import "scrivito-fancy-header";
@@ -28,9 +27,10 @@ import "scrivito-tabs";
 import "scrivito-blockquote";
 import "scrivito-swipeable";
 import "scrivito-content-lists";
+import "scrivito-eyecatcher";
+import "scrivito-eyecatcher/index.css";
 import "scrivito-undraw";
 import "scrivito-undraw/index.css";
-
 
 function importAll(r) {
   r.keys().forEach(r);
--- webpack.config.js
+++ webpack.config.js
@@ -127,6 +127,10 @@ function webpackConfig(env = {}) {
     },
     devServer: {
       port: 8080,
+      host: '0.0.0.0',
+      allowedHosts: [
+        '.local',
+      ],
       stats: "minimal",
       historyApiFallback: {
         rewrites: [
