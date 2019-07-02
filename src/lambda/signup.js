6a849d3cd9427d3e7c99c376c886d38aadaaf847
Antony Siegert <antony.siegert@infopark.de>
2019-07-02 21:22:04 +0200

    parse

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -57,7 +57,7 @@ module.exports.handler = (event, context, callback) => {
     if (response.statusCode < 300 || (bodyObj.status === 400 && bodyObj.title === "Member Exists")) {
       console.log("Added to list in Mailchimp subscriber list");
       callback(null, {
-        statusCode: JSON.stringify(response.statusCode),
+        statusCode: JSON.parse(response.statusCode),
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
