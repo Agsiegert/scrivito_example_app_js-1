02c6de204e18c695a42364e49809489f1620b6f3
Antony Siegert <antony.siegert@infopark.de>
2019-07-03 01:06:20 +0200

    cleanup editor config

--- src/Widgets/MailchimpSignUpWidget/MailchimpSignUpWidgetEditingConfig.js
+++ src/Widgets/MailchimpSignUpWidget/MailchimpSignUpWidgetEditingConfig.js
@@ -1,5 +1,4 @@
 import * as Scrivito from "scrivito";
-import HeadlineWidget from "../HeadlineWidget/HeadlineWidgetClass"
 
 Scrivito.provideEditingConfig("MailchimpSignUpWidget", {
   title: "Mailchimp Sign Up",
