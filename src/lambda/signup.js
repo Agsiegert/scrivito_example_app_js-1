d285d4e9391150736508d64644858008e08436e8
Antony Siegert <antony.siegert@infopark.de>
2019-07-02 23:31:33 +0200

    logs

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -53,6 +53,9 @@ module.exports.handler = (event, context, callback) => {
 
     console.log("Mailchimp body: " + JSON.stringify(bodyObj));
     console.log("Status Code: " + response.statusCode);
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
 
     if (response.statusCode < 300 || (bodyObj.status === 400 && bodyObj.title === "Member Exists")) {
       console.log("Added to list in Mailchimp subscriber list");
