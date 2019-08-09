6b6b07f1c8f85a6fd3fef9f14e88d371bbba5815
Antony Siegert <antony.siegert@infopark.de>
2019-08-09 20:16:31 +0200

    remove 0.0.0.0 host

--- webpack.config.js
+++ webpack.config.js
@@ -127,10 +127,6 @@ function webpackConfig(env = {}) {
     },
     devServer: {
       port: 8080,
-      host: '0.0.0.0',
-      allowedHosts: [
-        '.local',
-      ],
       stats: "minimal",
       historyApiFallback: {
         rewrites: [
