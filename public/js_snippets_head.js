75c91f4e3597e7c0a1bd33badb46786d1f9c974c
Antony Siegert <antony.siegert@infopark.de>
2019-11-18 16:10:07 +0100

    Adds GTM and upgrades Scrivito 1.10.0-rc1

--- public/js_snippets_head.js
+++ public/js_snippets_head.js
@@ -3,3 +3,8 @@
 // Don't forget to whitelist the URLs in the CSP at public/_headersCsp.json.
 
 // Google Analytics and Intercom are already taken care of by the app, so there's no need to add them here.
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PSM6L25');
