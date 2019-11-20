83dc39d354049a2a9140e4a0150336056f674a5c
Antony Siegert <antony.siegert@infopark.de>
2019-11-20 16:51:37 +0100

    move GTM script to test render blocking

--- src/prerenderContent/generateHtml.js
+++ src/prerenderContent/generateHtml.js
@@ -9,7 +9,11 @@ export default function generateHtml({
   return `<!DOCTYPE html>
 <html ${htmlAttributes}>
   <head>
-    <script src="/js_snippets_head.js"></script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PSM6L25');</script>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
