9eaef96ace8a58bc7c058df50e6c02d8cdd6fa35
Antony Siegert <antony.siegert@infopark.de>
2019-11-20 16:42:48 +0100

    Tests defer on GTM

--- public/js_snippets_head.js
+++ public/js_snippets_head.js
@@ -5,6 +5,6 @@
 // Google Analytics and Intercom are already taken care of by the app, so there's no need to add them here.
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PSM6L25');
